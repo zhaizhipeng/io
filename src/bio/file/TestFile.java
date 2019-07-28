c9e4022b97c0800fb25429b4323f7a7bc32e06f2
zhaizhipeng <hitpeng1230>
2019-07-28 08:28:09 +0800

    死磕File：判断操作

--- src/bio/file/TestFile.java
+++ src/bio/file/TestFile.java
@@ -93,4 +93,24 @@ public class TestFile {
         System.out.println("文件还存在么？"+file3.exists());
     }
 
+    /**
+     * 判断操作
+     */
+    @Test
+    public void testJudge(){
+        /**
+         * exists()			判断文件或文件夹是否存在，存在返回true，否则返回false
+         * isFile()			判断是否是一个文件，如果不存在，则返回false
+         * isDirectory()    判断是否是一个目录，如果不存在，则返回false
+         * isHidden()		判读是否是一个隐藏的文件或是否是隐藏的目录
+         * isAbsolute()		判断此抽象路径名是否为绝对路径
+         */
+        File file = new File("d:\\Test.txt");
+        System.out.println("文件存在么？" + file.exists());
+        System.out.println("是文件么？" + file.isFile());
+        System.out.println("是文件夹么？" + file.isDirectory());
+        System.out.println("是隐藏文件么？" + file.isHidden());
+        System.out.println("是绝对路径么？" + file.isAbsolute());
+    }
+
 }
